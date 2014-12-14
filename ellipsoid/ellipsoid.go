f4128526dada9307d7685043e8d908cdc057b3ce
YaGeek <yannick.heinrich@gmail.com>
2014-12-14 23:25:53 +0100

    Remove current unused member of ellipsoid

--- ellipsoid/ellipsoid.go
+++ ellipsoid/ellipsoid.go
@@ -3,17 +3,11 @@
 //go:generate gofmt -w ellipsoid_index.go
 package ellipsoid
 
-import (
-	"math"
-)
-
 //The informations contained in an Ellipsoid
 type Ellipsoid struct {
-	SemiMajorAxis float64
-	SemiMinorAxis float64
-	Flattening    float64
-	Excentricity  float64
-	Excentricity2 float64
+	SemiMajorAxis     float64
+	SemiMinorAxis     float64
+	InverseFlattening float64
 }
 
 func NewEllipsoidWithSemiAxis(a float64, b float64) *Ellipsoid {
@@ -22,22 +16,14 @@ func NewEllipsoidWithSemiAxis(a float64, b float64) *Ellipsoid {
 	p.SemiMajorAxis = a
 	p.SemiMinorAxis = b
 
-	p.Flattening = (a - b) / a
-	p.Excentricity2 = (math.Pow(a, 2) - math.Pow(b, 2)) / math.Pow(a, 2)
-	p.Excentricity = math.Sqrt(p.Excentricity2)
-
 	return p
 }
 
-func NewEllipsoidWithFlattening(a float64, f float64) *Ellipsoid {
+func NewEllipsoidWithInverseFlattening(a float64, inv_f float64) *Ellipsoid {
 	p := new(Ellipsoid)
 
 	p.SemiMajorAxis = a
-	p.Flattening = f
-
-	p.SemiMinorAxis = a * (1 - f)
-	p.Excentricity2 = (math.Pow(a, 2) - math.Pow(p.SemiMinorAxis, 2)) / math.Pow(a, 2)
-	p.Excentricity = math.Sqrt(p.Excentricity2)
+	p.InverseFlattening = inv_f
 
 	return p
 }
@@ -52,6 +38,6 @@ func NewEpsgEllipsoidWithSemiAxis(name string, epsg int, a float64, b float64) *
 	return &EpsgEllipsoid{Name: name, Epsg: epsg, Ellipsoid: NewEllipsoidWithSemiAxis(a, b)}
 }
 
-func NewEpsgEllipsoidWithFlattening(name string, epsg int, a float64, f float64) *EpsgEllipsoid {
-	return &EpsgEllipsoid{Name: name, Epsg: epsg, Ellipsoid: NewEllipsoidWithFlattening(a, f)}
+func NewEpsgEllipsoidWithInverseFlattening(name string, epsg int, a float64, f float64) *EpsgEllipsoid {
+	return &EpsgEllipsoid{Name: name, Epsg: epsg, Ellipsoid: NewEllipsoidWithInverseFlattening(a, f)}
 }
--- ellipsoid/generator/generator.go
+++ ellipsoid/generator/generator.go
@@ -18,7 +18,7 @@ package ellipsoid
 var datexCollection =[...]*EpsgEllipsoid{
 {{range .Collection}}
  {{if eq .SemiMinorAxis 0.0}}
- 	NewEpsgEllipsoidWithFlattening("{{.Name}}", {{.EPSG}}, {{printf "%f" .SemiMajorAxis}}, {{printf "%f" .InverseFlattening}} ),
+ 	NewEpsgEllipsoidWithInverseFlattening("{{.Name}}", {{.EPSG}}, {{printf "%f" .SemiMajorAxis}}, {{printf "%f" .InverseFlattening}} ),
  {{else}}
  	 NewEpsgEllipsoidWithSemiAxis( "{{.Name}}", {{.EPSG}}, {{printf "%f" .SemiMajorAxis}}, {{printf "%f" .SemiMinorAxis}} ) ,
  {{end}}
@@ -48,16 +48,17 @@ func main() {
 	defer resp.Body.Close()
 
 	data, _ := ioutil.ReadAll(resp.Body)
-	log.Println("Data:", string(data))
+
 	generatorData := &EllipsoidGeneratorData{Date: time.Now(), DataURL: DatexURI}
 
 	var collection []datex.Ellipsoid
 	err = json.Unmarshal(data, &collection)
 
-	log.Println("Data:", collection)
 	if err != nil {
 		log.Fatalln("Could not unmarshal ellipsoids:", err)
 	}
+
+	generatorData.Collection = collection
 
 	file, err := os.Create("ellipsoid_index.go")
 	if err != nil {
