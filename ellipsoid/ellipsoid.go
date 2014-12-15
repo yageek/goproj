3d9836afedf045ba62b6c6dc135cb761098e0a27
Yannick Heinrich <yannick.heinrich@gmail.com>
2014-12-15 14:06:43 +0100

    Make compilation works

--- ellipsoid/ellipsoid.go
+++ ellipsoid/ellipsoid.go
@@ -3,11 +3,17 @@
 //go:generate gofmt -w ellipsoid_index.go
 package ellipsoid
 
+import (
+	"math"
+)
+
 //The informations contained in an Ellipsoid
 type Ellipsoid struct {
 	SemiMajorAxis     float64
 	SemiMinorAxis     float64
 	InverseFlattening float64
+	Excentricity      float64
+	Excentricity2     float64
 }
 
 func NewEllipsoidWithSemiAxis(a float64, b float64) *Ellipsoid {
@@ -15,6 +21,9 @@ func NewEllipsoidWithSemiAxis(a float64, b float64) *Ellipsoid {
 
 	p.SemiMajorAxis = a
 	p.SemiMinorAxis = b
+	p.InverseFlattening = a / (a - b)
+	p.Excentricity2 = (math.Pow(a, 2) - math.Pow(b, 2)) / math.Pow(a, 2)
+	p.Excentricity = math.Sqrt(p.Excentricity2)
 
 	return p
 }
@@ -24,6 +33,10 @@ func NewEllipsoidWithInverseFlattening(a float64, inv_f float64) *Ellipsoid {
 
 	p.SemiMajorAxis = a
 	p.InverseFlattening = inv_f
+
+	p.SemiMinorAxis = a * (1 - 1/inv_f)
+	p.Excentricity2 = (math.Pow(a, 2) - math.Pow(p.SemiMinorAxis, 2)) / math.Pow(a, 2)
+	p.Excentricity = math.Sqrt(p.Excentricity2)
 
 	return p
 }
